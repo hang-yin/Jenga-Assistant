cdc253d950ad831161ef051581f4c66ee8feb0b0
Katie Hughes <katiereeshughes@gmail.com>
2022-11-03 14:24:16 -0500

    more tiny things

--- plan_execute/plan_execute/plan_and_execute.py
+++ plan_execute/plan_execute/plan_and_execute.py
@@ -2,29 +2,41 @@ import numpy as np
 import rclpy
 from moveit_msgs.action import MoveGroup
 from rclpy.action import ActionClient
-
+from moveit_msgs.srv import GetPositionIK
+from rclpy.callback_groups import ReentrantCallbackGroup
 
 
 class PlanAndExecute:
     def __init__(self, node):
-        # super().__init__("our_node")
-        # Create a client
         self.node = node
+        # Create a client
         self.node._action_client = ActionClient(self,
                                                 MoveGroup,
                                                 '/move_action')
-    def plan_to_position(self, start_pos, end_pos):
+        # Make it so we can call the IK service
+        self.cbgroup = ReentrantCallbackGroup()
+        self.node.IK = self.node.create_client(GetPositionIK,
+                                               "compute_ik",
+                                               callback_group = self.cbgroup)
+
+    def plan_to_position(self, start_pose, end_pos):
+        """Returns MoveGroup action from a start pose to an end end position"""
         mvg = MoveGroup()
         # 1. Call GetPositionIK.srv to get the joint states of final position 
+        # Something like this? await self.node.IK.call_async(end_pos) except idk how to get res
         # 2. Wait for service response (await?)
         # 3. Receive RobotState soln. soln.joint_state gives joint_state type msg
         # 4. Plug this into mvg.request.goal_constraints.joint_constraints (joint_state type)
         # 5. Return mvg action
         return mvg
-    def plan_to_orientation(self, start_pos, end_orientation):
-        pass 
-    def plan_to_pose(self,start_pos, end_pose):
-        pass
+    def plan_to_orientation(self, start_pose, end_orientation):
+        """Returns MoveGroup action from a start pose to an end orientation"""
+        mvg = MoveGroup()
+        return mvg
+    def plan_to_pose(self,start_pose, end_pose):
+        """Returns MoveGroup action from a start pose to an end pose (position + orientation)"""
+        mvg = MoveGroup()
+        return mvg
     def execute(self, mvg):
-        # CAll the client
+        """Takes a MoveGroup object, sends it through the client"""
         pass
