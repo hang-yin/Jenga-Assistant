9a44967a73c4ea851e70c4ea1a9285670dbb9c4b
Katie Hughes <katiereeshughes@gmail.com>
2022-11-03 14:02:15 -0500

    Guide for how to finish part2 in plan_and_execute python file

--- plan_execute/plan_execute/plan_and_execute.py
+++ plan_execute/plan_execute/plan_and_execute.py
@@ -13,13 +13,18 @@ class PlanAndExecute:
         self.node._action_client = ActionClient(self,
                                                 MoveGroup,
                                                 '/move_action')
-    def plan_to_position(self, position):
+    def plan_to_position(self, start_pos, end_pos):
         mvg = MoveGroup()
-        # mvg.request....... = ???
-        pass
-    def plan_to_orientation(self, orientation):
+        # 1. Call GetPositionIK.srv to get the joint states of final position 
+        # 2. Wait for service response (await?)
+        # 3. Receive RobotState soln. soln.joint_state gives joint_state type msg
+        # 4. Plug this into mvg.request.goal_constraints.joint_constraints (joint_state type)
+        # 5. Return mvg action
+        return mvg
+    def plan_to_orientation(self, start_pos, end_orientation):
         pass 
-    def plan_to_pose(self, pose):
+    def plan_to_pose(self,start_pos, end_pose):
         pass
-    def execute(self):
+    def execute(self, mvg):
+        # CAll the client
         pass
