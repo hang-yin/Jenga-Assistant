87173be31b3c90c242e5074a869ec1edff982819
hang-yin <yinhang0226@gmail.com>
2022-11-09 17:29:11 -0600

    Fixed style errors for plan_and_execute

--- plan_execute/plan_execute/plan_and_execute.py
+++ plan_execute/plan_execute/plan_and_execute.py
@@ -3,14 +3,14 @@ import rclpy
 from moveit_msgs.action import MoveGroup, ExecuteTrajectory
 from rclpy.action import ActionClient
 from moveit_msgs.srv import GetPositionIK, GetPlanningScene
-from moveit_msgs.msg import PositionIKRequest, Constraints, JointConstraint, PlanningScene, PlanningSceneComponents, CollisionObject
+from moveit_msgs.msg import PositionIKRequest, Constraints, JointConstraint, \
+                            PlanningScene, PlanningSceneComponents, CollisionObject
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 from sensor_msgs.msg import JointState
-from geometry_msgs.msg import Point, Quaternion, Pose, Transform
+from geometry_msgs.msg import Pose
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 from shape_msgs.msg import SolidPrimitive
-# import moveit movegroup
 
 
 class PlanAndExecute:
@@ -21,23 +21,23 @@ class PlanAndExecute:
                                                 MoveGroup,
                                                 '/move_action')
         self.node._execute_client = ActionClient(self.node,
-                                                ExecuteTrajectory,
-                                                '/execute_trajectory')
+                                                 ExecuteTrajectory,
+                                                 '/execute_trajectory')
         self.node.IK = self.node.create_client(GetPositionIK,
                                                "compute_ik",
-                                               callback_group = self.node.cbgroup)
+                                               callback_group=self.node.cbgroup)
         if not self.node.IK.wait_for_service(timeout_sec=5.0):
             raise RuntimeError('Timeout waiting for "IK" service to become available')
         self.node.planscene = self.node.create_client(GetPlanningScene,
-                                                    "get_planning_scene",
-                                                    callback_group = self.node.cbgroup)
+                                                      "get_planning_scene",
+                                                      callback_group=self.node.cbgroup)
         if not self.node.planscene.wait_for_service(timeout_sec=5.0):
             raise RuntimeError('Timeout waiting for "planscene" service to become available')
         self.move_group = self.node.movegroup
         self.node.js_sub = self.node.create_subscription(JointState,
-                                                    "/joint_states",
-                                                    self.js_callback,
-                                                    10)
+                                                         "/joint_states",
+                                                         self.js_callback,
+                                                         10)
         self.js = None
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self.node)
@@ -63,9 +63,9 @@ class PlanAndExecute:
         self.master_goal.request.pipeline_id = 'move_group'
         self.master_goal.request.max_velocity_scaling_factor = 1.0
         self.master_goal.request.max_acceleration_scaling_factor = 0.1
-    
+
     def printBlock(self, req):
-        new_str = (str(req)).replace(',',',\n')
+        new_str = (str(req)).replace(',', ',\n')
         self.node.get_logger().info(new_str)
 
     def js_callback(self, data):
@@ -83,8 +83,9 @@ class PlanAndExecute:
                                            tolerance_below=0.0001,
                                            weight=1.0)
             constraints.append(constraint_i)
-        self.master_goal.request.goal_constraints = [Constraints(name='', joint_constraints=constraints)]
-    
+        self.master_goal.request.goal_constraints = [Constraints(name='',
+                                                                 joint_constraints=constraints)]
+
     def createIKreq(self, end_pos, end_orientation):
         request = PositionIKRequest()
         request.group_name = self.master_goal.request.group_name
@@ -101,15 +102,16 @@ class PlanAndExecute:
 
     def getStartPose(self):
         startpose = Pose()
+        temp_frame_id = self.master_goal.request.workspace_parameters.header.frame_id
         t = self.tf_buffer.lookup_transform(
-                                            self.master_goal.request.workspace_parameters.header.frame_id,
+                                            temp_frame_id,
                                             'panda_hand_tcp',
                                             rclpy.time.Time())
         startpose.position.x = t.transform.translation.x
         startpose.position.y = t.transform.translation.y
         startpose.position.z = t.transform.translation.z
         startpose.orientation.x = t.transform.rotation.x
-        startpose.orientation.y =  t.transform.rotation.y
+        startpose.orientation.y = t.transform.rotation.y
         startpose.orientation.z = t.transform.rotation.z
         startpose.orientation.w = t.transform.rotation.w
         self.printBlock(startpose)
@@ -123,7 +125,8 @@ class PlanAndExecute:
             self.master_goal.request.start_state.joint_state = self.js
         else:
             request_start = self.createIKreq(start_pose.position, start_pose.orientation)
-            response_start = await self.node.IK.call_async(GetPositionIK.Request(ik_request = request_start))
+            request_temp = GetPositionIK.Request(ik_request=request_start)
+            response_start = await self.node.IK.call_async(request_temp)
             self.master_goal.request.start_state.joint_state = response_start.solution.joint_state
         self.master_goal.planning_options.plan_only = not execute
         request = self.createIKreq(end_pos.position, start_pose.orientation)
@@ -142,7 +145,8 @@ class PlanAndExecute:
             self.master_goal.request.start_state.joint_state = self.js
         else:
             request_start = self.createIKreq(start_pose.position, start_pose.orientation)
-            response_start = await self.node.IK.call_async(GetPositionIK.Request(ik_request = request_start))
+            request_temp = GetPositionIK.Request(ik_request=request_start)
+            response_start = await self.node.IK.call_async(request_temp)
             self.master_goal.request.start_state.joint_state = response_start.solution.joint_state
         self.master_goal.planning_options.plan_only = not execute
         request = self.createIKreq(start_pose.position, end_orientation.orientation)
@@ -155,9 +159,8 @@ class PlanAndExecute:
         else:
             return plan_result
 
-    
     async def plan_to_pose(self, start_pose, end_pose, execute):
-        """Returns MoveGroup action from a start pose to an end pose (position + orientation)"""
+        """Returns MoveGroup action from a start to end pose (position + orientation)"""
         self.node.get_logger().info("Plan to Pose")
         if not start_pose:
             start_pose = self.getStartPose()
@@ -165,11 +168,12 @@ class PlanAndExecute:
         else:
             self.node.get_logger().info("NEW START")
             request_start = self.createIKreq(start_pose.position, start_pose.orientation)
-            response_start = await self.node.IK.call_async(GetPositionIK.Request(ik_request = request_start))
+            request_temp = GetPositionIK.Request(ik_request=request_start)
+            response_start = await self.node.IK.call_async(request_temp)
             self.master_goal.request.start_state.joint_state = response_start.solution.joint_state
             self.node.get_logger().info(response_start)
             self.node.get_logger().info(self.master_goal.request.start_state.joint_state)
-        
+
         self.master_goal.planning_options.plan_only = not execute
         request = self.createIKreq(end_pose.position, end_pose.orientation)
         plan_result = await self.plan(request)
@@ -182,7 +186,7 @@ class PlanAndExecute:
             return plan_result
 
     async def plan(self, IKrequest):
-        response = await self.node.IK.call_async(GetPositionIK.Request(ik_request = IKrequest))
+        response = await self.node.IK.call_async(GetPositionIK.Request(ik_request=IKrequest))
         joint_names = response.solution.joint_state.name
         joint_positions = np.array(response.solution.joint_state.position)
         self.fill_constraints(joint_names, joint_positions)
@@ -195,15 +199,17 @@ class PlanAndExecute:
     async def execute(self, plan_result):
         self.node.get_logger().info("Wait for execute client")
         self.node._execute_client.wait_for_server()
-        execute_future = await self.node._execute_client.send_goal_async(ExecuteTrajectory.Goal(trajectory=plan_result.result.planned_trajectory))
+        traj_goal = ExecuteTrajectory.Goal(trajectory=plan_result.result.planned_trajectory)
+        execute_future = await self.node._execute_client.send_goal_async(traj_goal)
         execute_result = await execute_future.get_result_async()
         return execute_result
 
     async def place_block(self, pos):
         self.node.get_logger("Place Block")
         scene_request = PlanningSceneComponents()
-        scene_request.components=0
-        scene = await self.node.planscene.call_async(GetPlanningScene.Request(components=scene_request))
+        scene_request.components = 0
+        temp_scene_request = GetPlanningScene.Request(components=scene_request)
+        scene = await self.node.planscene.call_async(temp_scene_request)
         self.printBlock(scene)
         scene = scene.scene
         scene.robot_state.joint_state = self.js
