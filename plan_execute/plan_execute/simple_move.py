235614634b17c19ad07d662aa358392a44f09511
Liz Metzger <elizabethmetzger2024@u.northwestern.edu>
2022-11-09 16:53:45 -0600

    no random prints

--- plan_execute/plan_execute/simple_move.py
+++ plan_execute/plan_execute/simple_move.py
@@ -3,9 +3,10 @@ from rclpy.node import Node
 from enum import Enum, auto
 from plan_execute_interface.srv import GoHere, Place
 from plan_execute.plan_and_execute import PlanAndExecute
-from moveit_msgs.action import MoveGroup
-from geometry_msgs.msg import Point, Quaternion, Pose
+from geometry_msgs.msg import Pose
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+
+
 class State(Enum):
     """
     Current state of the system.
@@ -32,11 +33,10 @@ class Test(Node):
         self.freq = 100.
         self.cbgroup = MutuallyExclusiveCallbackGroup()
         self.timer = self.create_timer(1./self.freq, self.timer_callback, callback_group=self.cbgroup)
-        self.movegroup = None # Fill this in later lol
+        self.movegroup = None
         self.go_here = self.create_service(GoHere, 'go_here', self.go_here_callback)
         self.place = self.create_service(Place, 'place', self.place_callback)
         self.PlanEx = PlanAndExecute(self)
-
         self.state = State.START
         self.ct = 0
         self.goal_pose = Pose()
@@ -47,8 +47,6 @@ class Test(Node):
         self.start_pose = request.start_pose
         self.goal_pose = request.goal_pose
         self.execute = request.execute
-        print("START POSE")
-        print(self.start_pose)
         l = len(self.start_pose)
         if l == 0:
             self.start_pose = None
@@ -67,7 +65,6 @@ class Test(Node):
     
     def place_callback(self, request, response):
         self.block_pose = request.place
-        print(f'block_pose{type(self.block_pose)}')
         self.state = State.PLACE
         return response
 
@@ -83,13 +80,9 @@ class Test(Node):
             self.future = await self.PlanEx.plan_to_pose(self.start_pose, self.goal_pose, self.execute)
             # self.future = await self.PlanEx.plan_to_position(self.start_pose, self.goal_pose, self.execute)
             # self.future = await self.PlanEx.plan_to_orientation(self.start_pose, self.goal_pose, self.execute)
-            print(type(self.future))
-            print("MAIN LOOP:", self.future)
         if self.state == State.PLACE:
-            self.get_logger().info("test1")
             self.state = State.IDLE
             self.PlanEx.place_block(self.block_pose)
-            self.get_logger().info("test")
 
 
 def test_entry(args=None):
