205cbb0182ad981dd24b35451f72bca774e0cc4e
Liz Metzger <elizabethmetzger2024@u.northwestern.edu>
2022-11-09 19:10:58 -0600

    more doc strings

--- plan_execute/plan_execute/plan_and_execute.py
+++ plan_execute/plan_execute/plan_and_execute.py
@@ -14,7 +14,13 @@ from shape_msgs.msg import SolidPrimitive
 
 
 class PlanAndExecute:
+    """
+    Execute functions based off of service calls to return either return or execute a move plan.
+
+
+    """
     def __init__(self, node):
+        """Initilize varibles and states, fill messages, and create callback groups."""
         self.node = node
         self.node.cbgroup = MutuallyExclusiveCallbackGroup()
         self.node._action_client = ActionClient(self.node,
@@ -65,14 +71,16 @@ class PlanAndExecute:
         self.master_goal.request.max_acceleration_scaling_factor = 0.1
 
     def printBlock(self, req):
+        """Make a string of a request or message then log it to the terminal"""
         new_str = (str(req)).replace(',', ',\n')
         self.node.get_logger().info(new_str)
 
     def js_callback(self, data):
-        """Save js (sensor_msgs/JointStates type)."""
+        """Save js with the joint state data (sensor_msgs/JointStates type)."""
         self.js = data
 
     def fill_constraints(self, joint_names, joint_positions):
+        """Fill the joint constraints field with information from the joint states message"""
         self.node.get_logger().info("Filling Constraints")
         constraints = []
         for n, i in enumerate(joint_names):
@@ -86,9 +94,9 @@ class PlanAndExecute:
             constraints.append(constraint_i)
         self.master_goal.request.goal_constraints = [Constraints(name='',
                                                                  joint_constraints=constraints)]
-        self.node.get_logger().info("Finished Filling")
 
     def createIKreq(self, end_pos, end_orientation):
+        """Create an IK message filled with info from the goal pose and orientation"""
         request = PositionIKRequest()
         request.group_name = self.master_goal.request.group_name
         request.robot_state.joint_state.name = self.js.name
@@ -103,6 +111,7 @@ class PlanAndExecute:
         return request
 
     def getStartPose(self):
+        """Calculate the postion and orientation of the robot based on the tf frames"""
         startpose = Pose()
         temp_frame_id = self.master_goal.request.workspace_parameters.header.frame_id
         t = self.tf_buffer.lookup_transform(
@@ -116,7 +125,6 @@ class PlanAndExecute:
         startpose.orientation.y = t.transform.rotation.y
         startpose.orientation.z = t.transform.rotation.z
         startpose.orientation.w = t.transform.rotation.w
-        self.printBlock(startpose)
         return startpose
 
     async def plan_to_position(self, start_pose, end_pos, execute):
@@ -148,7 +156,6 @@ class PlanAndExecute:
 
     async def plan_to_orientation(self, start_pose, end_orientation, execute):
         """Return MoveGroup action from a start pose to an end orientation."""
-        self.node.get_logger().info("\n\n\n\n\nHERE\n\n\n\n\n\n")
         if not start_pose:
             start_pose = self.getStartPose()
             self.master_goal.request.start_state.joint_state = self.js
@@ -180,7 +187,6 @@ class PlanAndExecute:
             start_pose = self.getStartPose()
             self.master_goal.request.start_state.joint_state = self.js
         else:
-            self.node.get_logger().info("NEW START")
             request_start = self.createIKreq(start_pose.position, start_pose.orientation)
             request_temp = GetPositionIK.Request(ik_request=request_start)
             response_start = await self.node.IK.call_async(request_temp)
@@ -221,7 +227,6 @@ class PlanAndExecute:
         joint_positions = np.array(joint_state.position)
         self.fill_constraints(joint_names, joint_positions)
         self.node._action_client.wait_for_server()
-        self.printBlock(self.master_goal)
         plan = await self.node._action_client.send_goal_async(self.master_goal)
         plan_result = await plan.get_result_async()
         return plan_result
@@ -240,7 +245,6 @@ class PlanAndExecute:
         scene_request.components = 0
         temp_scene_request = GetPlanningScene.Request(components=scene_request)
         scene = await self.node.planscene.call_async(temp_scene_request)
-        self.printBlock(scene)
         scene = scene.scene
         scene.robot_state.joint_state = self.js
         primepose = Pose()
@@ -255,5 +259,4 @@ class PlanAndExecute:
         collision.primitives = [prime]
         collision.primitive_poses = [primepose]
         scene.world.collision_objects = [collision]
-        self.printBlock(scene)
         self.node.block_pub.publish(scene)
