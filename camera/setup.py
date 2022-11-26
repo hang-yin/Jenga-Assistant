b80240def2fc915bc124171bc88f0a2e342b411a
Liz Metzger <elizabethmetzger2024@u.northwestern.edu>
2022-11-25 20:47:40 -0600

    made a node to send a static transform between the camera and panda_link0

--- camera/setup.py
+++ camera/setup.py
@@ -24,7 +24,8 @@ setup(
     entry_points={
         'console_scripts': [
             'cam = camera.realsense:main',
-            'cali = camera.calibrate:main'
+            'cali = camera.calibrate:main',
+            'broadcast = camera.broadcast_transform:main'
         ],
     },
 )
