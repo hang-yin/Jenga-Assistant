e4cb1ee32d3108b75a4835c2894ec1a5ef955d58
Katie Hughes <katiereeshughes@gmail.com>
2022-11-30 22:41:57 -0600

    cleaning up

--- camera/camera/realsense.py
+++ camera/camera/realsense.py
@@ -1,43 +1,19 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import CompressedImage
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import CameraInfo
 import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
-import sys
-# np.set_printoptions(threshold=sys.maxsize)
 import pyrealsense2 as rs2
 from enum import Enum, auto
 from std_srvs.srv import Empty
-from geometry_msgs.msg import PoseArray, Pose, Quaternion
-from math import sqrt, sin, cos
+from geometry_msgs.msg import Pose
+from math import sqrt, dist
 from tf2_ros import TransformBroadcaster
 from geometry_msgs.msg import TransformStamped
-from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
 from std_msgs.msg import Bool
-from math import dist
 
-
-def angle_axis_to_quaternion(theta, axis):
-    """
-    Convert from angle-axis of rotation to a quaternion.
-    This is taken from the tf activity we did in class.
-    Args:
-        theta:  rotation angle, in radians
-        axis: the rotational axis. This will be normalized
-    Returns
-    -------
-    A Quaternion corresponding to the rotation
-    """
-    magnitude = sqrt(axis[0]**2 + axis[1]**2 + axis[2]**2)
-    normalized = [v/magnitude for v in axis]
-    sinTheta2 = sin(theta/2.0)
-    return Quaternion(x=normalized[0]*sinTheta2,
-                      y=normalized[1]*sinTheta2,
-                      z=normalized[2]*sinTheta2,
-                      w=cos(theta/2.0))
 
 class State(Enum):
     """The current state of the brick."""
@@ -79,26 +55,27 @@ class Cam(Node):
                                                         10)
 
         self.piece_pub = self.create_publisher(Pose, 'jenga_piece', 10)
-        self.corners_pub = self.create_publisher(PoseArray, 'jenga_corners', 10)
         self.scan = self.create_service(Empty, "scan", self.scan_service_callback)
         self.stop = self.create_service(Empty, "stop", self.stop_service_callback)
         self.calib = self.create_service(Empty, "findtower", self.calib_service_callback)
         self.br = CvBridge()
 
         # Initialize the transform broadcaster
-        # Static bc block only gets published once!
         self.tf_broadcaster = TransformBroadcaster(self)
         self.brick = TransformStamped()
         self.frame_brick = "brick"
         self.frame_camera ='camera_link'
 
+        # These will be updated from my subscribers to camera data
         self.color_frame = None
         self.depth_frame = None
         self.intrinsics = None
 
+        # What "band" I look at upon startup
         self.band_start = 570
         self.band_width = 20
 
+        # For smoothing the image
         kernel_size = 25
         self.kernel = np.ones((kernel_size,kernel_size),np.uint8)
 
@@ -117,13 +94,11 @@ class Cam(Node):
         self.max_scan = 1000
         self.scan_step = 0.5
 
-        self.corner_threshold = 0.01
-        self.corner_scale = 100
-
-        # How large a contour is to consider it as relevant
-        self.object_area_threshold = 10000
+        # Contour area threshold for detecting the table
+        self.table_area_threshold = 10000
+        # Contour area threshold for detecting a piece sticking out
         self.piece_area_threshold = 2000
-        # Size of about 1 piece when it's on the top
+        # Little bit less than the area of 1 piece when it's on the top
         self.top_area_threshold = 16000
 
         self.piece_depth = 0.029 # 3 cm
@@ -133,9 +108,13 @@ class Cam(Node):
         self.ct = 0
         self.avg_piece = Pose()
 
+        # For averaging (median-ing?) the coordinates of the found piece
         self.piece_x = []
         self.piece_y = []
         self.piece_z = []
+
+        # For storing the coordinates of the top of the tower
+        self.top_corners = None
 
         # cv2.namedWindow('Mask')
         # cv2.createTrackbar('kernel size', 'Mask', kernel_size, 100, self.kernel_trackbar)
@@ -147,10 +126,6 @@ class Cam(Node):
         cv2.createTrackbar('band width', 'Color' , self.band_width, 100, self.band_width_tb)
         cv2.createTrackbar('band start', 'Color' , self.band_start, 1000, self.band_start_tb)
 
-        # cv2.namedWindow('Corners')
-        # cv2.createTrackbar('corner threshold*100', 'Corners',
-        #                    int(self.corner_threshold*self.corner_scale),
-        #                    self.corner_scale, self.corner_trackbar)
 
     def sqx_trackbar(self, val):
         self.sq_orig[0] = val
@@ -180,9 +155,6 @@ class Cam(Node):
     def kernel_trackbar(self, val):
         self.kernel = np.ones((val,val),np.uint8)
 
-    def corner_trackbar(self, val):
-        self.corner_threshold = val/self.corner_scale
-
     def scan_service_callback(self, _, response):
         """ Make the camera scan for pieces sticking out """
         # Only valid if the tower and table have a height
@@ -200,7 +172,7 @@ class Cam(Node):
         self.get_logger().info("Pause Scanning")
         return response
     
-    def piece_found_cb(self, data):
+    def piece_found_cb(self, d_):
         # Stop publishing tf data!!
         self.get_logger().info('Brick found')
         self.state = State.PAUSED
@@ -240,16 +212,12 @@ class Cam(Node):
         current_frame = self.br.imgmsg_to_cv2(data)
         self.depth_frame = current_frame
 
-        # depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(current_frame, alpha=0.3), cv2.COLORMAP_JET)
-        # cv2.imshow("im 13 and this is deep", depth_colormap)
-
     def get_mask(self, care_about_square = True):
         # Do this in case the subscriber somehow updates in the middle of the function?
         depth_cpy = np.array(self.depth_frame)
-        # self.get_logger().info(f"SHAPE: {depth_cpy.shape}")
 
         # Only keep stuff that's within the appropriate depth band.
-        depth_mask = cv2.inRange(depth_cpy, self.band_start, self.band_start+self.band_width)
+        depth_mask = cv2.inRange(np.array(depth_cpy), self.band_start, self.band_start+self.band_width)
         # This operation helps to remove "dots" on the depth image.
         # Kernel higher dimensional = smoother. It's also less important if camera is farther away.
         depth_mask = cv2.morphologyEx(depth_mask, cv2.MORPH_CLOSE, self.kernel)
@@ -262,18 +230,10 @@ class Cam(Node):
         square = cv2.inRange(square, 1, 255)
         # Cropping the depth_mask so that only what is within the square remains.
         depth_mask = cv2.bitwise_and(depth_mask, depth_mask, mask=square)
-        # Draw a rectangle on a copy of the depth_mask to visualize where it is.
-        # rectangle = cv2.rectangle(np.array(depth_mask),
-        #                           self.rect[0][0], self.rect[0][2],
-        #                           (255,0,0), 2)
-        # cv2.imshow("Mask", rectangle)
 
         # Find the contours of this cropped mask to help locate tower.
         contours, _ = cv2.findContours(depth_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        centroids = []
-        areas = []
-        large_contours = []
-        # self.get_logger().info(f"Number of countours: {len(contours)}")
+        centroids, areas, large_contours = [], [], []
         for c in contours: 
             M = cv2.moments(c)
             area = cv2.contourArea(c)
@@ -287,16 +247,13 @@ class Cam(Node):
                     large_contours.append(c)
             except: 
                 pass
-        max_centroid = None
-        largest_area = None
-        box_area = None
-        centroid_pose = None
-        box = None
-        corner_array = None
+        largest_area, centroid_pose, corner_array = None, None, None
+        max_centroid, box, box_area = None, None, None
         if len(areas) != 0: 
             # There is something large in the image.
             largest_index = np.argmax(areas)
             largest_area = areas[largest_index]
+            self.get_logger().info(f"LARGEST AREA: {largest_area}")
             max_centroid = centroids[largest_index]
             max_contour = large_contours[largest_index]
             centroid_depth = depth_cpy[max_centroid[1]][max_centroid[0]]
@@ -310,7 +267,7 @@ class Cam(Node):
             centroid_pose.position.z = centroid_deprojected[2]/1000.
 
             # Try finding the corners of the largest thing
-            box, box_area, corner_array = self.get_box(max_contour, depth_cpy)
+            box, box_area, corner_array = self.get_box(max_contour,depth_cpy)
 
         # Display the images
         color_rect = cv2.rectangle(np.array(self.color_frame),
@@ -322,9 +279,7 @@ class Cam(Node):
             drawn_contours = cv2.drawContours(drawn_contours, [box], 0, (255,0,0), 3)
             if care_about_square and (largest_area/box_area < 0.9):
                     # The contour is not really a rectangle and therefore doesn't work well
-                    largest_area = None
-                    centroid_pose = None
-                    corner_array = None
+                    largest_area, centroid_pose, corner_array = None, None, None
         cv2.imshow('Color', drawn_contours)
 
         cv2.waitKey(1)
@@ -334,39 +289,56 @@ class Cam(Node):
         min_rect = cv2.minAreaRect(max_contour)
         box = cv2.boxPoints(min_rect)
         box = np.intp(box)
+        # Save original box area to test if the contour is a good fit
         box_area = dist(box[0],box[1])*dist(box[1],box[2])
-        # self.get_logger().info(f"Box coords: {box}")
-        # dy = box[1][1]-box[0][1]
-        # dx = box[1][0]-box[0][0]
-        # self.get_logger().info(f"dy: {dy}, dx: {dx}")
-        # angle = np.arctan2(dy,dx)
-        # self.get_logger().info(f"Angle: {angle}")
-        # centroid_pose.orientation = angle_axis_to_quaternion(angle, [0, 1, 0])
-        corner_array = PoseArray()
-        corner_array.header.stamp = self.get_clock().now().to_msg()
+        # Rescale so that the corners are actually on the physical object
+        # (otherwise looking up the depth doesn't work)
+        percent_shrink = 0.75
+        new_w = sqrt(percent_shrink)*min_rect[1][0]
+        new_h = sqrt(percent_shrink)*min_rect[1][1]
+        new_rect = (min_rect[0], (new_w,new_h), min_rect[2])
+        box = cv2.boxPoints(new_rect)
+        box = np.intp(box)
+        corner_array = []
         for corner in box:
             corner_depth = depth_frame[corner[1]][corner[0]]
-            # self.get_logger().info(f"Corner: {corner_depth}")
             corner_deprojected = rs2.rs2_deproject_pixel_to_point(self.intrinsics,
                                                                     corner,
                                                                     corner_depth)
-            # self.get_logger().info(f"DEPROJECTED Corner:{corner_deprojected}")
-            cornerPose = Pose()
-            cornerPose.position.x = corner_deprojected[0]/1000.
-            cornerPose.position.y = corner_deprojected[1]/1000.
-            cornerPose.position.z = corner_deprojected[2]/1000.
-            corner_array.poses.append(cornerPose)
-        # self.get_logger().info(f"CORNER ARRAY: {corner_array}")
+            # Save data as transform stamped as that is what will be sent
+            corner_position = TransformStamped()
+            corner_position.transform.translation.x =corner_deprojected[0]/1000.
+            corner_position.transform.translation.y =corner_deprojected[1]/1000.
+            corner_position.transform.translation.z =corner_deprojected[2]/1000.
+            corner_array.append(corner_position)
         return box, box_area, corner_array
+    
+    def publish_top(self):
+        if self.top_corners is not None:
+            for n,corner in enumerate(self.top_corners):
+                corner.header.stamp = self.get_clock().now().to_msg()
+                corner.header.frame_id = self.frame_camera
+                corner.child_frame_id = 'tower_corner_'+str(n)
+                # Transform is all 0s if deproject fails. Don't publish these
+                if not ((corner.transform.translation.x == 0) and
+                        (corner.transform.translation.y == 0) and
+                        (corner.transform.translation.z == 0)):
+                    self.tf_broadcaster.sendTransform(corner)
 
     def timer_callback(self):
+
+        # Every time you need to publish the top of the tower
+        self.publish_top()
+
         if self.state == State.WAITING:
+            # Pause while we wait for frames
             self.get_logger().info("Waiting for frames...")
             wait_for = [self.intrinsics, self.depth_frame, self.color_frame]
             if all(w is not None for w in wait_for):
                 self.state = State.PAUSED
 
         elif self.state == State.PAUSED:
+            # Just print out the camera data
             largest_area, _, _ = self.get_mask()
 
         elif self.state == State.FINDTOP:
@@ -388,8 +360,10 @@ class Cam(Node):
                     self.get_logger().info(f"depth: {self.band_start+self.band_width},"+
                                            f"area: {largest_area}\n")
                     self.tower_top = self.band_start+self.band_width
-                    for c in corner_array.poses:
-                        self.get_logger().info(f"Corner:{c.position}")
+                    # for c in corner_array:
+                    #     self.get_logger().info(f"Corner:{c}")
+                    # This will begin publishing to the tf tree
+                    self.top_corners = corner_array
                     # Go down past the top pieces, or else this will also be detected as table.
                     # UNCOMMENT THESE LATER
                     # self.scan_index = self.tower_top + self.band_width
@@ -419,7 +393,7 @@ class Cam(Node):
             # The contour of the table will not be a square.
             largest_area, _, _ = self.get_mask(care_about_square=False)
             if largest_area:
-                if largest_area > self.object_area_threshold:
+                if largest_area > self.table_area_threshold:
                     # We believe there is an object at this depth
                     self.get_logger().info("FOUND TABLE!!!!!!")
                     self.get_logger().info(f"depth: {self.band_start+self.band_width},"+
@@ -428,6 +402,7 @@ class Cam(Node):
                     self.scan_index = self.tower_top + self.band_width
                     self.band_start = self.tower_top + self.band_width
                     self.state = State.PAUSED
+
         elif self.state == State.SCANNING:
             # Keep scanning downwards
             self.band_start = self.scan_index
@@ -445,20 +420,15 @@ class Cam(Node):
                         # self.get_logger().info("Piece (?) detected")
                         # self.get_logger().info(f"Depth: {self.band_start}, area: {largest_area}")
                         # self.get_logger().info(f"Pose in camera frame: {centroid_pose}")
-                        # Avg the pose
+                        # Record the position
                         self.piece_x.append(centroid_pose.position.x)
                         self.piece_y.append(centroid_pose.position.y)
                         self.piece_z.append(centroid_pose.position.z)
-                        # self.avg_piece.position.x += centroid_pose.position.x
-                        # self.avg_piece.position.y += centroid_pose.position.y
-                        # self.avg_piece.position.z += centroid_pose.position.z
                         self.ct += 1
                         # Stay at same scan level
                         self.scan_index -= self.scan_step
                     else:
-                        # self.avg_piece.position.x /= float(self.avg_frames)
-                        # self.avg_piece.position.y /= float(self.avg_frames)
-                        # self.avg_piece.position.z /= float(self.avg_frames)
+                        # take median to avoid weird jumping behavior
                         self.avg_piece.position.x = np.median(self.piece_x)
                         self.avg_piece.position.y = np.median(self.piece_y)
                         self.avg_piece.position.z = np.median(self.piece_z)
@@ -482,18 +452,13 @@ class Cam(Node):
                         # self.avg_piece = Pose()
                         self.state = State.PUBLISHPIECE
         elif self.state == State.PUBLISHPIECE:
-            # self.get_logger().info('PUBLISH TFS!')
             # Continue to publish camera image
             _,_,_ = self.get_mask()
-            #create tf between the tag and the rotated frame using avg_piece
+            # Create tf between the tag and the rotated frame using avg_piece
             self.brick.header.stamp = self.get_clock().now().to_msg()
             self.brick.header.frame_id = self.frame_camera
             self.brick.child_frame_id = self.frame_brick
             self.tf_broadcaster.sendTransform(self.brick)
-
-
-
-
 
 
 def main(args=None):
